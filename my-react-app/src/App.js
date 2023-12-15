ec860152326932c49de2330431e1539b59d9f0c2
--rani-vyas <--ranivyas2003@gmail.com>
2023-12-15 15:12:29 +0530

    in calendar changes

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -25,7 +25,7 @@ function App() {
         </a>
   </header>*/}
 
-<ZipPostalCode/>
+<WeekCalendar/>
     </div>
   );
 }
--- my-react-app/src/calendarApp/mycalendar.js
+++ my-react-app/src/calendarApp/mycalendar.js
@@ -5,17 +5,16 @@ import './calendar.css';
 
 export function WeekCalendar (){
  // debugger;
-
 const [Current,setCurrent] = useState(moment());
 console.log(Current)
 
 const handleChangepre = () =>{
-  //debugger
+  debugger
   setCurrent(Current.clone().subtract(1, "week"));
 }
 
 const handleChangenext = () =>{
- //debugger;
+ debugger;
   setCurrent(Current.clone().add(1,'week'));
 }
 
@@ -25,13 +24,13 @@ const handleChangenext = () =>{
 <div style={{border:'3px solid black',paddingBottom:'20%', marginTop:'30px', marginBottom:'50px'}}>
 <p className='text-center' style={{fontSize:'2rem'}}>Calendar</p>
 <h2>{moment().format("LLL")}</h2>
-      <button onClick={handleChangepre} style={{marginRight:'90%',padding:'5px' , borderRadius:'5px' , fontSize:'2rem',border:'none'}} >&lt;</button>
-      <button onClick={handleChangenext} style={{padding:'5px',borderRadius:'5px',fontSize:'2rem', border:'none'}} >&gt;</button>
+      <button onClick={handleChangepre} style={{marginRight:'90%',borderRadius:'5px' , border:'none'}} >&lt;</button>
+      <button onClick={handleChangenext} style={{mraginLeft:'90%',borderRadius:'5px', border:'none'}} >&gt;</button>
   <table style={{border:'1px solid black',borderRadius:'10px',marginTop:'10px', width:'100%'}}>
     <thead>
     <tr>
     {Current.localeData()._weekdaysShort.map((item, index) => (
-            <th key={index} style={{backgroundColor:'lightblue'}} >
+            <th key={index} style={{backgroundColor:'lightblue', border:'1px solid black'}} >
               {item}
             </th>
           ))}
